eaca0de7449f374bc0718d3ad0b35d04cb43577f
Joren Lauwers <jorenl@mit.edu>
2018-12-13 10:24:32 -0500

    the code

---
+++ colors.py
@@ -0,0 +1,20 @@
+import random
+
+colors = [
+    "darkpurple",
+    "yellow",
+    "orange",
+    "red",
+    "lightpink",
+    "darkpink",
+    "black",
+    "lightpurple",
+    "darkgreen",
+    "lightgreen",
+    "white",
+    "blue"
+]
+
+while True:
+    print(random.choice(colors))
+    input()
